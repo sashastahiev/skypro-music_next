3b911ca283415bc4b05714db73f9c7552b7c621b
sashastahiev <vebium@mail.ru>
2026-06-29 19:39:33 +0300

    commit hw6 домашка

--- skypro-music/jest.config.js
+++ skypro-music/jest.config.js
@@ -1,17 +1,17 @@
-module.exports = {
+/**@type {import('jest').Config}*/
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.jest.json',
-    },
-  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|scss|sass)$': 'identity-obj-proxy',
   },
   transform: {
-    '^.+.tsx?$': ['ts-jest']
+    '^.+.tsx?$': ['ts-jest',{
+      tsconfig: 'tsconfig.jest.json',
+    },]
   }
 };
+
+module.exports = config;
